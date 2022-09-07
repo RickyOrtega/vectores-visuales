20b85741632f0d26e20b873fb6c8a792d7e8610f
Ricky Rhonaldo Ortega Vital <ortegavitalricky@gmail.com>
2022-09-06 22:19:38 -0500

    Hechas las ventanas.
    Ya hice las ventanas, están hermosas pero aún les falta funcionalidad.

---
+++ Arrays/src/modelo/Array.java
@@ -0,0 +1,275 @@
+package modelo;
+
+import javax.swing.*;
+
+public class Array {
+
+    private int A[];
+
+    private int tamagno;
+    private int c;
+
+    public Array(int tamagno) {
+        this.tamagno = tamagno;
+        this.c = 0;
+        this.A = new int[tamagno];
+    }
+
+    public int getTamagno() {
+        return tamagno;
+    }
+
+    public void setTamagno(int tamagno) {
+        this.tamagno = tamagno;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public int insercionInicio(int x) {
+        if (c == tamagno) {
+            return 0;
+        } else {
+            if (c == 0) {
+                A[0] = x;
+            } else {
+                for (int k = c; k > 0; k--) {
+                    A[k] = A[k - 1];
+                }
+                A[0] = x;
+            }
+            c++;
+            return 1;
+        }
+    }
+
+    public int insercionFinal(int x) {
+        if (c == tamagno) {
+            return 0;
+        } else {
+            A[c] = x;
+            c++;
+            return 1;
+        }
+    }
+
+    //Recibe como parámetros un número x que se sitúa en un índice
+    public int insercionReferencia(int x, int indice) {
+        int aux;
+        if (c == tamagno) {
+            return 0;
+        } else {
+            if (c == 0) {
+                A[indice] = x;
+            } else {
+                for (int k = c; k > indice; k--) {
+                    A[k] = A[k - 1];
+                }
+                A[indice] = x;
+            }
+            c++;
+            return 1;
+        }
+    }
+
+    public int eliminarInicio() {
+        if (c == 0) {
+            return 0;
+        } else {
+            if (c > 1) {
+                int k;
+                for (k = 0; k < c - 1; k++) {
+                    A[k] = A[k + 1];
+                }
+            }
+        }
+        c--;
+        return 1;
+    }
+
+    public int eliminarFinal() {
+        if (c == 0) {
+            return 0;
+        } else {
+            c--;
+        }
+        return 1;
+    }
+
+    public int eliminarReferencia(int pos) {
+        if (c == 0) {
+            return 0;
+        } else {
+            if (0 <= pos && pos <= c - 1) {
+                if (pos != 0) {
+                    int k;
+                    for (k = pos; k < c - 1; k++) {
+                        A[k] = A[k + 1];
+                    }
+                }
+                c--;
+            } else {
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    public int busquedaSecuencial(int dato) {
+        int pos = -1;
+
+        if (c == 0) {
+            return pos;
+        } else {
+            int k;
+            for (k = 0; k < c; k++) {
+                if (dato == A[k]) {
+                    pos = k;
+                    break;
+                }
+            }
+        }
+        return pos;
+    }
+
+    public int busquedaBinaria() {
+        int dato;
+        int inferior;
+        int superior;
+        int mitad;
+        int i;
+        boolean flag = false;
+
+        dato = Integer.parseInt(JOptionPane.showInputDialog("Digite el número a buscar: "));
+
+        inferior = 0;
+        superior = 5;//la cantidad de elementos
+        i = 0;
+
+        mitad = (inferior + superior) / 2;
+
+        while (inferior <= superior && i < 5) {
+            if (A[mitad] == dato) {
+                flag = true;
+                break;
+            }
+            if (A[mitad] > dato) {
+                superior = mitad;
+                mitad = (inferior + superior) / 2;
+            }
+            if (A[mitad] < dato) {
+                inferior = mitad;
+                mitad = (inferior + superior) / 2;
+            }
+            i++;
+        }
+        if (flag == true) {
+            return mitad;
+        } else {
+            return -1;
+        }
+    }
+
+    public int busquedaBinaria(int dato) {
+        int inferior;
+        int superior;
+        int mitad;
+        int i;
+        boolean flag = false;
+
+        inferior = 0;
+        superior = 5;//la cantidad de elementos
+        i = 0;
+
+        mitad = (inferior + superior) / 2;
+
+        while (inferior <= superior && i < 5) {
+            if (A[mitad] == dato) {
+                flag = true;
+                break;
+            }
+            if (A[mitad] > dato) {
+                superior = mitad;
+                mitad = (inferior + superior) / 2;
+            }
+            if (A[mitad] < dato) {
+                inferior = mitad;
+                mitad = (inferior + superior) / 2;
+            }
+            i++;
+        }
+        if (flag == true) {
+            return mitad;
+        } else {
+            return -1;
+        }
+    }
+
+    //Muestra el vector
+    public void recorrer() {
+        if (c == 0) {
+            
+        } else {
+            for (int k = 0; k < this.c; k++) {
+                System.out.print(this.A[k] + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    //Ordenamientos, nadie los pidió pero quiero y puedo jajaja
+    //Método Burbuja
+    public void ordenamientoBurbuja() {
+        int aux;
+        for (int i = 0; i < (c - 1); i++) {
+            for (int x = 0; x < (c - 1); x++) {
+                if (A[x] > A[x + 1]) {
+                    aux = A[x];
+                    A[x] = A[x + 1];
+                    A[x + 1] = aux;
+                }
+            }
+        }
+        JOptionPane.showMessageDialog(null, "Ordenamiento realizado");
+    }
+
+    //Ordenamiento por inserción
+    public void ordenamientoInsercion() {
+        int posicion;
+        int aux;
+        for (int i = 0; i < c; i++) {
+            posicion = i;
+            aux = A[i];
+
+            while ((posicion > 0) && (A[posicion - 1] > aux)) {
+                A[posicion] = A[posicion - 1];
+                posicion--;
+            }
+            A[posicion] = aux;
+        }
+        JOptionPane.showMessageDialog(null, "Ordenamiento realizado");
+    }
+
+    //Ordenamiento por Selección
+    public void ordenamientoSeleccion() {
+        int min;
+        int aux;
+        for (int i = 0; i < c; i++) {
+            min = i;
+            for (int x = i + 1; x < c; x++) {
+                if (A[x] < A[min]) {
+                    min = x;
+                }
+            }
+            aux = A[i];
+            A[i] = A[min];
+            A[min] = aux;
+        }
+        JOptionPane.showMessageDialog(null, "Ordenamiento realizado");
+    }
+}
---
+++ Arrays/src/vistas/Presentación.java
@@ -0,0 +1,66 @@
+package vistas;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+import javax.swing.UIManager;
+
+public class Presentación extends javax.swing.JFrame {
+
+    public Presentación() {
+        initComponents();
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("VECTORES");
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel2.setText("<html>Presentado por:  <br><br>>Ricky Rhonaldo Ortega Vital <br>>Ronaldinho Ortega Ruiz <br>>Ruben Perdomo</html>");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 28, Short.MAX_VALUE)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 372, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(53, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Presentación().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    // End of variables declaration//GEN-END:variables
+}
